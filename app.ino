feac7c8a82193568864e0d181016be1e39cc8d57
M <owjs3901@naver.com>
2019-11-03 16:12:21 +0900

    acc sensor

--- app.ino
+++ app.ino
@@ -1,6 +1,10 @@
+#include <Adafruit_Sensor.h>
+
+#include <Adafruit_LIS3DH.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial blueTooth(10,11);
+
+SoftwareSerial blueTooth(10, 11);
 
 #define button0 8
 #define button1 9
@@ -9,22 +13,51 @@ SoftwareSerial blueTooth(10,11);
 #define btGND 12
 
 
+Adafruit_LIS3DH sensor;
 
-SoftwareSerial bt(bt_rx,bt_tx)
+void setup()
+{
+	Serial.begin(9600);
+	blueTooth.begin(9600);
+	pinMode(button0, INPUT);
+	pinMode(button1, INPUT);
 
-void setup(){
-    Serial.begin(9600);
-    blueTooth.begin(9600);
-    pinMode(button0,INPUT);
-    pinMode(button1,INPUT);
+	sensor.begin();
 
-    pinMode(btVCC,OUTPUT);
-    pinMode(btGND,OUTPUT);
-    digitalWrite(btVCC,HIGH);
-    digitalWrite(btGND,LOW);
+	pinMode(btVCC, OUTPUT);
+	pinMode(btGND, OUTPUT);
+	digitalWrite(btVCC, 1);
+	digitalWrite(btGND, LOW);
 }
 
-void loop(){
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	int finger0 = digitalRead(button0);
+	int finger1 = digitalRead(button1);
+	Serial.write("x : ");
+	Serial.write(sensor.x);
+	Serial.write("\n");
+	Serial.write("y : ");
+	Serial.write(sensor.y);
+	Serial.write("\n");
+	Serial.write("z : ");
+	Serial.write(sensor.z);
+	Serial.write("\n");
 
-	
+	if (blueTooth.available())
+		Serial.write(blueTooth.read());
+	//if(Serial.available())
+	//bt.write(Serial.read());
+
+	if (finger0 == HIGH && finger1 == LOW)
+		blueTooth.write("1;0;1;1;0;\n");
+	else if (finger0 == LOW && finger1 == HIGH)
+		blueTooth.write("1;0;0;1;1;\n");
+	else if (finger0 == HIGH && finger1 == HIGH)
+		blueTooth.write("1;0;1;1;1;\n");
+	else
+		blueTooth.write("1;0;0;1;0;\n");
+
+	delay(45);
 }
