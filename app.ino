451f5b310642e034c19c874acbe3998e6d57d229
M <owjs3901@naver.com>
2019-11-03 14:15:54 +0900

    setup

--- app.ino
+++ app.ino
@@ -1,16 +1,30 @@
 #include <SoftwareSerial.h>
 
-#define bt_tx
-#define bt_rx
+SoftwareSerial blueTooth(10,11);
+
+#define button0 8
+#define button1 9
+
+#define btVCC 13
+#define btGND 12
+
+
 
 SoftwareSerial bt(bt_rx,bt_tx)
 
+void setup(){
+    Serial.begin(9600);
+    blueTooth.begin(9600);
+    pinMode(button0,INPUT);
+    pinMode(button1,INPUT);
 
-void setup()
-{
+    pinMode(btVCC,OUTPUT);
+    pinMode(btGND,OUTPUT);
+    digitalWrite(btVCC,HIGH);
+    digitalWrite(btGND,LOW);
 }
 
-void loop()
-{
+void loop(){
+
 	
 }
