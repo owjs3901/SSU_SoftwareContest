ba4a06e5de003f6e591a52c603ac11935a6ea5ef
M <owjs3901@naver.com>
2019-11-29 10:49:06 +0900

    final

--- app.ino
+++ app.ino
@@ -1,63 +1,61 @@
-#include <Adafruit_Sensor.h>
 
-#include <Adafruit_LIS3DH.h>
+#include "Wire.h"
+#include "SPI.h"
+#include "SparkFunLIS3DH.h"
+
+// #include <Adafruit_Sensor.h>
+
+// #include <Adafruit_LIS3DH.h>
+
 #include <SoftwareSerial.h>
 
 
 SoftwareSerial blueTooth(10, 11);
 
-#define button0 8
-#define button1 9
 
-#define btVCC 13
-#define btGND 12
-
-
-Adafruit_LIS3DH sensor;
+LIS3DH sensor;
 
 void setup()
 {
-	Serial.begin(9600);
-	blueTooth.begin(9600);
-	pinMode(button0, INPUT);
-	pinMode(button1, INPUT);
+  Serial.begin(9600);
+  blueTooth.begin(38400);
+  // pinMode(button0, INPUT);
+  // pinMode(button1, INPUT);
 
-	sensor.begin();
-
-	pinMode(btVCC, OUTPUT);
-	pinMode(btGND, OUTPUT);
-	digitalWrite(btVCC, 1);
-	digitalWrite(btGND, LOW);
+  sensor.begin();
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
-	int finger0 = digitalRead(button0);
-	int finger1 = digitalRead(button1);
-	Serial.write("x : ");
-	Serial.write(sensor.x);
-	Serial.write("\n");
-	Serial.write("y : ");
-	Serial.write(sensor.y);
-	Serial.write("\n");
-	Serial.write("z : ");
-	Serial.write(sensor.z);
-	Serial.write("\n");
+  // put your main code here, to run repeatedly:
+  // int finger0 = digitalRead(button0);
+  // int finger1 = digitalRead(button1);
+  Serial.print("TEST");
+  float x=sensor.readFloatAccelX();
+  float y=sensor.readFloatAccelY();
+  float z=sensor.readFloatAccelZ();
+  Serial.print("x : ");
+  Serial.print(sensor.readFloatAccelX());
+  Serial.print("\n");
+  Serial.print("y : ");
+  Serial.print(sensor.readFloatAccelY());
+  Serial.print("\n");
+  Serial.print("z : ");
+  Serial.print(sensor.readFloatAccelZ());
+  Serial.print("\n");
+  if (blueTooth.available())
+    Serial.write(blueTooth.read());
+  if(Serial.available())
+    blueTooth.write(Serial.read());
+  blueTooth.write("1;0;1;1;0;");
+    
+  blueTooth.write(x);
+  blueTooth.write(";");
+  blueTooth.write(y);
+  blueTooth.write(";");
+  blueTooth.write(z);
+  blueTooth.write(";");
+  blueTooth.write("\n");
 
-	if (blueTooth.available())
-		Serial.write(blueTooth.read());
-	//if(Serial.available())
-	//bt.write(Serial.read());
-
-	if (finger0 == HIGH && finger1 == LOW)
-		blueTooth.write("1;0;1;1;0;\n");
-	else if (finger0 == LOW && finger1 == HIGH)
-		blueTooth.write("1;0;0;1;1;\n");
-	else if (finger0 == HIGH && finger1 == HIGH)
-		blueTooth.write("1;0;1;1;1;\n");
-	else
-		blueTooth.write("1;0;0;1;0;\n");
-
-	delay(45);
+  delay(45);
 }
